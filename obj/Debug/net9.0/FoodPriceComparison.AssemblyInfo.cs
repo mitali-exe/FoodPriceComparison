8cdeb6666913e34abc94651aebc4ce2c65e61e3e
Mitali Srivastav <mitalisrivastav615@gmail.com>
2025-09-24 22:46:58 +0530

    Second commit

--- obj/Debug/net9.0/FoodPriceComparison.AssemblyInfo.cs
+++ obj/Debug/net9.0/FoodPriceComparison.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodPriceComparison")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+617de281550705d18044d8742142b7b124c78694")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodPriceComparison")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodPriceComparison")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
